ca3b6a39f142ff2b545ec0f96baca1414aa17050
Adel Kamel <adelkamel1982@gmail.com>
2018-11-10 00:54:51 +0100

    adding

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const express = require("express");
-const app 		= express();
+const express 				= require('express');
+const mongoose 				= require('mongoose');
+const bodyParser 			= require('body-parser');
+const methodOverride 	= require('method-override')
+const app 						= express();
+
+// Database
+mongoose.connect('mongodb://localhost/todoapp',{useNewUrlParser: true}).then(() => {
+	console.log('Connecté à la base de données');
+}); 
 
 // Template engine
 app.set('view engine', 'pug');
+
+// Body Parser
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// Methode override
+app.use(methodOverride('_method'));
 
 // Routes
 const index = require('./routes/index');
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,51 @@
-const express = require("express");
+const express = require('express');
 const router 	= express.Router();
 
-router.get('/', (req, res) => {
-	res.render('index');
+const Todo 		=	require('../models/Todo');
+
+// Récupération des données 
+router.get('/', (req, res) => { // On a une requête de type GET à la racine
+	Todo.find({}).then((todos) => { // On ce sert du modèle Todo et on récupère tout les documents de la collection
+		res.render('index', { // On passe le tableau todos dans la vue 
+			todos: todos
+		});
+	});
 });
+
+// Récupération de la valeur de l'input
+router.post('/add', (req, res) => {
+	const newTodo = new Todo({
+		title: req.body.title // Attribut name de l'input
+	})
+	newTodo.save().then(() => {
+		res.redirect('/'); // Redirige la page vers la racine
+	});
+});
+
+// Delete d'une valeur 
+router.delete('/delete/:id', (req,res) => {
+	Todo.findOneAndDelete(req.params.id, () => { // Fonction callback autre qu'avec le .then
+		res.redirect('/');
+	})
+})
+
+// Edition d'une valeur
+router.get('/update/:id', (req, res) => {
+	Todo.findById(req.params.id, (err, todo) => {
+		console.log(todo);
+		res.render('update', {
+			todo: todo
+		});
+	})
+})
+
+router.put('/update/:id', (req,res) => {
+	Todo.findByIdAndUpdate(req.params.id, {title: req.body.title}, (err) => {
+		if(err) {
+			console.log(err);
+		}
+		res.redirect('/');
+	})
+})
 
 module.exports = router;
