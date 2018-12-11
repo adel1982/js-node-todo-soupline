1c907ec8ccbc03cb09827a5b6223b5a5336faa07
Adel Kamel <adelkamel1982@gmail.com>
2018-12-11 23:17:10 +0100

    Adding route

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,10 @@
 const express = require('express');
 const router 	= express.Router();
 
-const Todo 		=	require('../models/Todo');
+const indexController = require('../controllers/index');
 
-// Récupération des données 
-router.get('/', (req, res) => { // On a une requête de type GET à la racine
-	Todo.find({}).then((todos) => { // On ce sert du modèle Todo et on récupère tout les documents de la collection
-		res.render('index', { // On passe le tableau todos dans la vue 
-			todos: todos
-		});
-	});
-});
+router.get('/', indexController.getIndex);
 
-// Récupération de la valeur de l'input
-router.post('/add', (req, res) => {
-	const newTodo = new Todo({
-		title: req.body.title // Attribut name de l'input
-	})
-	newTodo.save().then(() => {
-		res.redirect('/'); // Redirige la page vers la racine
-	});
-});
-
-// Delete d'une valeur 
-router.delete('/delete/:id', (req,res) => {
-	Todo.findOneAndDelete(req.params.id, () => { // Fonction callback autre qu'avec le .then
-		res.redirect('/');
-	})
-})
-
-// Edition d'une valeur
-router.get('/update/:id', (req, res) => {
-	Todo.findById(req.params.id, (err, todo) => {
-		console.log(todo);
-		res.render('update', {
-			todo: todo
-		});
-	})
-})
-
-router.put('/update/:id', (req,res) => {
-	Todo.findByIdAndUpdate(req.params.id, {title: req.body.title}, (err) => {
-		if(err) {
-			console.log(err);
-		}
-		res.redirect('/');
-	})
-})
+router.post('/ajouter', indexController.postIndex);
 
 module.exports = router;
