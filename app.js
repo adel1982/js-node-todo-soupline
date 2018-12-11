605d401f5c7639ceea41300ee00d7befe390994d
Adel Kamel <adelkamel1982@gmail.com>
2018-12-11 23:18:19 +0100

    core app modification

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const express 				= require('express');
 const mongoose 				= require('mongoose');
 const bodyParser 			= require('body-parser');
-const methodOverride 	= require('method-override')
-const app 						= express();
+// const methodOverride 	= require('method-override')
 
-// Database
-mongoose.connect('mongodb://localhost/todoapp',{useNewUrlParser: true}).then(() => {
-	console.log('Connecté à la base de données');
-}); 
 
-// Template engine
+// Connect to Database
+mongoose.connect('mongodb://admin:ePcTwRq6yVZ7@ds161518.mlab.com:61518/js-node-todo-soupline',{ useNewUrlParser: true } ).then(() => {
+	console.log('Connecté à la base de donnée');
+})
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(methodOverride('_method'));
+
 app.set('view engine', 'pug');
 
-// Body Parser
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-
-// Methode override
-app.use(methodOverride('_method'));
-
-// Routes
-const index = require('./routes/index');
-app.use('/', index);
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Serveur lancé sur le port ${port}`));
