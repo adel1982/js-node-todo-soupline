fed81f677501f88a5bfc7f3d75224dd525a6afdf
Adel Kamel <adelkamel1982@gmail.com>
2018-12-12 21:09:10 +0100

    delete task

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,6 @@ const Todo = require('../models/Todo');
 const getIndex = (req, res) => {
 	Todo.find({}, (err, todos) => {
 		if(err) console.log(err);
-		console.log(todos);
 		res.render('index', {
 			todos: todos
 		});
@@ -20,7 +19,16 @@ const postIndex = (req, res) => {
 	});
 }
 
+const deleteIndex = (req, res) => {
+	const { id } = req.params
+	Todo.findByIdAndDelete(id, (err) => {
+		if(err) console.log(err);
+		res.redirect('/');
+	})
+}
+
 module.exports = {
 	getIndex: getIndex,
-	postIndex: postIndex
+	postIndex: postIndex,
+	deleteIndex: deleteIndex
 }
